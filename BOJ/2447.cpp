fa40988ab0d1afafc2dbd0e92b27ae16c59a1b9e
오지훈 <doer.tail@gmail.com>
2023-06-25 18:07:06 +0900

    solved:   BOJ/2447

--- BOJ/2447.cpp
+++ BOJ/2447.cpp
@@ -3,13 +3,10 @@
 
 #define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
 
-#ifdef ONLINE_JUDGE
-#define DEBUG
-#endif
-
 using namespace std;
+char board[6562][6562];
 
-void func(int N);
+void func(int i, int j, int N);
 
 int main(void){
     FAST_IO
@@ -17,18 +14,35 @@ int main(void){
     int k;
     cin >> k;
 
-    func(k);
+    for(int i=0; i<k; i++)
+        for(int j=0; j<k; j++)
+            board[i][j] = ' ';
+
+    func(0, 0, k);
+
+    for(int i=0; i<k; i++){
+        for(int j=0; j<k; j++){
+            cout  << board[i][j];
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
 
-void func(int N){
-    if(N == 1){
-        cout << "*";
+void func(int x, int y, int k){
+    if(k==1){
+        board[x][y] = '*';
         return;
     }
 
-    for(int i=0; i<N; i++){
-        
+    int len = k/3;
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(i == 1 && j == 1)
+                continue;
+            func(x + i*len, y+ j*len, len);
+        }
     }
 }
