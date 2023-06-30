d0cff2c2048fc48fc3ba336e7f55bdaa9cbfe9a6
오지훈 <doer.tail@gmail.com>
2023-06-30 16:49:01 +0900

    solved:   BOJ/15656

---
+++ BOJ/15656.cpp
@@ -0,0 +1,41 @@
+// 15655: N과 M (7)
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+
+void solution();
+vector<int> vec;
+vector<int> ptr;
+int N, M;
+
+int main(void){
+    FAST_IO
+    
+    cin >> N >> M;
+
+    for(int i=0; i<N; i++){
+        int n;
+        cin >> n;
+        vec.push_back(n);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    solution();
+
+    return 0;
+}
+
+void solution(){
+    if(ptr.size() == M){
+        for(int n:ptr)
+            cout << n <<' ';
+        cout << '\n';
+        return;
+    }
+    for(int j=0; j<N; j++){
+        ptr.push_back(vec[j]);
+        solution();
+        ptr.pop_back();
+    }
+}
