c99002d7f21b526dc212044687cc7f5e36a5691e
오지훈 <doer.tail@gmail.com>
2023-07-21 16:25:27 +0900

    solved:   BOJ/11931

---
+++ BOJ/11931.cpp
@@ -0,0 +1,20 @@
+// 11931: 수 정렬하기 4
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+
+int main(void){
+    FAST_IO
+
+    int N; cin >> N;
+    int arr[1000001];
+    for(int i=0; i<N; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr+N, greater<int>());
+
+    for(int i=0; i<N; i++)
+        cout <<  arr[i] <<"\n";
+    
+    return 0;
+}
