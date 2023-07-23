0e6a8cf328a5ca4f2cc989d3f653dd17aecd57ab
오지훈 <doer.tail@gmail.com>
2023-07-24 01:42:06 +0900

    solved:   BOJ/14890

---
+++ BOJ/14890.cpp
@@ -0,0 +1,17 @@
+// 14890: 경사로
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+
+#define DEBUG
+#ifdef ONLINE_JUDGE
+#endif
+
+
+int main(void){
+    FAST_IO
+    
+    
+    return 0;
+}
+
