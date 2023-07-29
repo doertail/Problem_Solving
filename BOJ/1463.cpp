ca4ffeb4d595f3e7be3b95587edd819f3f148ce6
오지훈 <doer.tail@gmail.com>
2023-07-30 01:25:30 +0900

    modified:   BOJ/1463.cpp

--- BOJ/1463.cpp
+++ BOJ/1463.cpp
@@ -4,39 +4,20 @@ using namespace std;
 
 #define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
 
-queue<pair<int, int>> Q;
-int arr[3000001];
+int d[10000005];
+int N;
 
 int main(void){
     FAST_IO
-    int N;
     cin >> N;
-
-    Q.push({1, 0});
-    arr[1] = 0;
-    if(N == 1){
-        cout << 0 <<"\n";
-        return 0;
-    }
-    while(arr[N] == 0){
-        pair<int, int> frt = Q.front(); Q.pop();
-        if(frt.first >= N) continue;
-
-        if(!arr[frt.first+1]){
-            arr[frt.first+1] = frt.second+1;
-            Q.push({frt.first+1, frt.second+1});
-        }
-        if(!arr[frt.first*2]){
-            arr[frt.first*2] = frt.second+1;
-            Q.push({frt.first*2, frt.second+1});
-        }
-        if(!arr[frt.first*3]){
-            arr[frt.first*3] = frt.second+1;
-            Q.push({frt.first*3, frt.second+1});
-        }
+    d[1] = 0;
+    for(int i=2; i<=N; i++){
+        d[i] = d[i-1]+1;
+        if(i%2==0) d[i] = min(d[i], d[i/2]+1);
+        if(i%3==0) d[i] = min(d[i], d[i/3]+1);
     }
 
-    cout << arr[N]<<"\n";
-    
+    cout << d[N];
+
     return 0;
 }
