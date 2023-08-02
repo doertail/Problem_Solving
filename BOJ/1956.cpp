36700f4a13df3c26a8448061ab97c99419b9dfb9
오지훈 <doer.tail@gmail.com>
2023-08-02 15:33:05 +0900

    solved:   BOJ/1956

---
+++ BOJ/1956.cpp
@@ -0,0 +1,29 @@
+// 1965: 상자 넣기
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+
+int a[1001];
+int d[1001];
+int main(void){
+    FAST_IO
+    fill(d, d+1001, 1);
+
+    int n; cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+
+    for(int i=0; i<n; i++){
+        int num = a[i];
+        for(int j=0; j<i; j++){
+            // cout <<i <<' '<< j << '\n';
+            if(num <= a[j])
+                continue;
+            if(d[i] < d[j] + 1)
+                d[i] = d[j] + 1;
+        }
+    }
+    cout << *max_element(d, d+n);
+
+    return 0;
+}
