be0869b4028b48cabbc10887e150ad3e96ee9059
오지훈 <doer.tail@gmail.com>
2023-08-09 02:36:29 +0900

    solved:   BOJ/1062

--- BOJ/1062.cpp
+++ BOJ/1062.cpp
@@ -3,11 +3,13 @@
 #define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
 using namespace std;
 string w[51];
-
+bool is_readable(string str, int num);
 int main(void){
     FAST_IO
-    int num = 1<<('a'-'a') | 1<<('n'-'a') | 1<<('t'-'a') | 1<<('i'-'a') | 1<<('c'-'a');
-    int MAX=0, arr[26], N, K; 
+    int N, K, ANS=0, arr[26], num, tmp; 
+    // ref: a, c, i, n, t를 갖고있음.
+    int ref = 1<<('a'-'a') | 1<<('c'-'a') | 1<<('i'-'a') | 1<<('n'-'a') | 1<<('t'-'a');
+    
     cin >> N >> K;    
 
     for(int i=0; i<N; i++)
@@ -17,29 +19,31 @@ int main(void){
     fill(arr, arr+K, 0);
 
     do{
-        int ref = 0, tmp = 0;
+        num = 0, tmp = 0;
         for(int i=0; i<26; i++){
-            if(arr[i]==0)
-                ref = ref|(1<<i);
+            if(arr[i] == 0)
+                num = num|(1<<i); // arr에서 0인거를 비트로 인식하고, num에 기록.
         }
-        if((ref & num) != num)
+
+        if((num & ref) != ref) // num이 a, c, i, n, t를 가지고 있는지를 체크. 없으면 다음!
             continue;
 
         for(int i=0; i<N; i++){
-            bool flag = true;
-            
-            for(int j=4; j<w[i].length()-4; j++){
-                if((ref & (1<<(w[i][j]-'a'))) != (1<<(w[i][j]-'a'))){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) tmp++;
+            if(is_readable(w[i], num))
+                tmp++;
         }
-        MAX = max(tmp, MAX);
+        ANS = max(tmp, ANS);
     }while(next_permutation(arr, arr+26));
     
-    cout << MAX;
+    cout << ANS;
 
     return 0;
+}
+// 단어 읽을 수 있는지 체크.
+bool is_readable(string str, int num){
+    for(char c: str){
+        if((num & (1<<(c-'a'))) != (1<<(c-'a'))) // string에 있는 문자가 num에 없음.
+            return false;
+    }
+    return true;
 }
