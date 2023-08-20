c43abb335e5c8102770b7374d134f65b4f77fa57
오지훈 <doer.tail@gmail.com>
2023-08-20 18:11:55 +0900

    modified:   BOJ/template.cpp

--- BOJ/template.cpp
+++ BOJ/template.cpp
@@ -1,6 +1,8 @@
 // Number: Name
 #include <bits/stdc++.h>
 #define FAST_IO ios_base::sync_with_stdio(0); cin.tie(NULL);
+#define ll long long
+#define pair pair<int, int>
 using namespace std;
 
 int main(void){
